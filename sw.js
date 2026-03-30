766ded1f33b52322a510946fbd13b308f427b5d5
NULVEC <cotoastacio@icloud.com>
2026-03-30 01:02:32 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 // FlowEX Service Worker v2.0
 const CACHE = 'flowex-v2.0';
 const STATIC = [
-  '/',
-  '/index.html',
-  '/manifest.json',
+  './',
+  './index.html',
+  './manifest.json',
   'https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Mono:wght@300;400&display=swap',
 ];
 
